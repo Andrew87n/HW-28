7c084cf0fbb5162a66a9da4bf8d60c279ae78110
Andrew87n <andlop@ukr.net>
2026-03-05 19:27:25 +0200

    12

--- app.js
+++ app.js
@@ -32,3 +32,46 @@ listRef.addEventListener("click", (event) => {
     bookArray.splice(index, 1)
     renderArray()
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const nameRef = document.querySelector("#username");
+const passwordRef = document.querySelector("#password");
+const saveRef = document.querySelector("#saveBtn");
+
+saveRef.addEventListener("click", (event) => {
+    localStorage.clear() 
+    event.preventDefault
+    nameRef.value = ""
+    passwordRef.value = ""
+})
+
+nameRef.addEventListener("input", (event) => {
+    const inputName = event.target.value.trim()
+    localStorage.setItem("name", inputName)
+})
+
+passwordRef.addEventListener("input", (event) => {
+    const inputPassword = event.target.value.trim()
+    localStorage.setItem("password", inputPassword)
+})
+
+function checkStorage (){
+    const getName = localStorage.getItem("name");
+    const getPassword = localStorage.getItem("password");
+    nameRef.value = getName
+    passwordRef.value = getPassword
+}
+
+checkStorage()
